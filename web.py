6234808aeba7d4201dfe21b0cba1119e410465e8
Keerthi <keerthireddykamjula@gmail.com>
2025-02-09 22:03:07 +0530

    Updated link in web.py

--- web.py
+++ web.py
@@ -4,9 +4,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title='Prediction of Disease Outbreakss', layout='wide',page_icon="👩‍⚕")
-diabetes_model=pickle.load(open(r"C:\Users\keert\OneDrive\Documents\Predictions of Disease Outbreak\training_models\diabetes_model.sav",'rb'))
-heart_model=pickle.load(open(r"C:\Users\keert\OneDrive\Documents\Predictions of Disease Outbreak\training_models\heart_model.sav",'rb'))
-parkinsons_model=pickle.load(open(r"C:\Users\keert\OneDrive\Documents\Predictions of Disease Outbreak\training_models\parkinsons_model.sav",'rb'))
+diabetes_model=pickle.load(open(r"https://drive.google.com/file/d/1967snOy6nFGvKEaDjI7Pp8PM_cBCkQml/view?usp=sharing",'rb'))
+heart_model=pickle.load(open(r"https://drive.google.com/file/d/1QRF1nuxXtvHD4ObQbC2ZHPpSyZsIC04Q/view?usp=sharing",'rb'))
+parkinsons_model=pickle.load(open(r"https://drive.google.com/file/d/1Cd7qzc3Nt7rjpRvDsKpTmMLGF3J3sXfZ/view?usp=sharing",'rb'))
                                    
 with st.sidebar:
     selected= option_menu('prediction of disease outbreak system',['Diabetes Prediction','Heart Disease Prediction','Parkinsons Prediction'],menu_icon='hospital-fill',icons=['activity','heart','person'],default_index=0)
